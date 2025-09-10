651ea2bd803ed2bcd8b5cf7c0b356e3ab0b4a3ce
mayconmazai <79338981+mayconmazai@users.noreply.github.com>
2025-09-10 09:21:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/mz1art/',
+    base: '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
